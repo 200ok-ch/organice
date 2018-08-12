b83dbcaae1ebb03493dc9b4e1ec83b99b0a14894
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-08-12 15:02:51 -0700

    Significant progress on exporting a table back to raw text

--- src/lib/export_org.js
+++ src/lib/export_org.js
@@ -1,4 +1,69 @@
-const exportOrg = (headers, todoKeywordSets) => {
+import _ from 'lodash';
+
+const linkPartToRawText = linkPart => {
+  if (!!linkPart.getIn(['contents', 'title'])) {
+    return `[[${linkPart.getIn(['contents', 'uri'])}][${linkPart.getIn(['contents', 'title'])}]]`;
+  } else {
+    return `[[${linkPart.getIn(['contents', 'uri'])}]]`;
+  }
+};
+
+const tablePartToRawText = tablePart => {
+  const rowHeights = tablePart.get('contents').map(row => (
+    Math.max(...row.get('contents').map(cell => (
+      (_.countBy(cell.get('rawContents'))['\n'] || 0) + 1
+    )))
+  )).toJS();
+
+  const numColumns = tablePart.getIn(['contents', 0, 'contents']).size;
+  const columnWidths = _.times(numColumns).map(columnIndex => (
+    Math.max(...tablePart.get('contents').map(row => (
+      Math.max(
+        ...row
+          .getIn(['contents', columnIndex, 'rawContents'])
+          .split('\n')
+          .map(line => line.trim().length)
+      )
+    )))
+  ));
+
+  const rowStrings = _.dropRight(_.flatten(tablePart.get('contents').map((row, rowIndex) => {
+    const rowHeight = rowHeights[rowIndex];
+
+    const contentRows = _.times(rowHeight).map(lineIndex => (
+      row.get('contents').map((cell, columnIndex) => (
+        cell
+          .get('rawContents')
+          .split('\n')[lineIndex]
+          .trim()
+          .padEnd(columnWidths[columnIndex], ' ')
+      )).toJS().join('  | ')
+    )).map(contentRow => `| ${contentRow}  |`);
+
+    const separator = '|' + columnWidths.map(columnWidth => '-'.repeat(columnWidth + 3)).join('+') + '|';
+
+    return contentRows.concat(separator);
+  }).toJS()));
+
+  return rowStrings.join('\n');
+};
+
+export const attributedStringToRawText = parts => {
+  return parts.map(part => {
+    switch (part.get('type')) {
+    case 'text':
+      return part.get('contents');
+    case 'link':
+      return linkPartToRawText(part);
+    case 'table':
+      return tablePartToRawText(part);
+    default:
+      return '';
+    }
+  }).join('');
+};
+
+export default (headers, todoKeywordSets) => {
   let configContent = '';
   if (!todoKeywordSets.get(0).get('default')) {
     configContent = todoKeywordSets.map(todoKeywordSet => {
@@ -31,5 +96,3 @@ const exportOrg = (headers, todoKeywordSets) => {
 
   return configContent + headerContent;
 };
-
-export default exportOrg;
