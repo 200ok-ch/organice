b155e5226a757f70f0ec73186444049f627dbce1
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-08-12 15:34:36 -0700

    Update table export to include formatted text

--- src/lib/export_org.js
+++ src/lib/export_org.js
@@ -8,6 +8,25 @@ const linkPartToRawText = linkPart => {
   }
 };
 
+const formattedAttributedStringText = parts => {
+  return parts.map(part => {
+    switch (part.get('type')) {
+    case 'text':
+      return part.get('contents');
+    case 'link':
+      if (part.getIn(['contents', 'title'])) {
+        return part.getIn(['contents', 'title']);
+      } else {
+        return part.getIn(['contents', 'uri']);
+      }
+    case 'table':
+      return '';
+    default:
+      return '';
+    }
+  }).join('');
+};
+
 const tablePartToRawText = tablePart => {
   const rowHeights = tablePart.get('contents').map(row => (
     Math.max(...row.get('contents').map(cell => (
@@ -17,27 +36,28 @@ const tablePartToRawText = tablePart => {
 
   const numColumns = tablePart.getIn(['contents', 0, 'contents']).size;
   const columnWidths = _.times(numColumns).map(columnIndex => (
-    Math.max(...tablePart.get('contents').map(row => (
-      Math.max(
-        ...row
-          .getIn(['contents', columnIndex, 'rawContents'])
-          .split('\n')
-          .map(line => line.trim().length)
-      )
-    )))
+    Math.max(...tablePart.get('contents').map(row => {
+      const content = row.getIn(['contents', columnIndex, 'contents']);
+      const formattedText = formattedAttributedStringText(content);
+      const lineLengths = formattedText.split('\n').map(line => line.trim().length);
+      return Math.max(...lineLengths);
+    }))
   ));
 
   const rowStrings = _.dropRight(_.flatten(tablePart.get('contents').map((row, rowIndex) => {
     const rowHeight = rowHeights[rowIndex];
 
     const contentRows = _.times(rowHeight).map(lineIndex => (
-      row.get('contents').map((cell, columnIndex) => (
-        cell
-          .get('rawContents')
-          .split('\n')[lineIndex]
-          .trim()
-          .padEnd(columnWidths[columnIndex], ' ')
-      )).toJS().join('  | ')
+      row.get('contents').map((cell, columnIndex) => {
+        const content = cell.get('contents');
+        const formattedText = formattedAttributedStringText(content);
+        const formattedLineLengths = formattedText.split('\n').map(line => line.trim().length);
+        const line = cell.get('rawContents').split('\n')[lineIndex].trim();
+
+        const padCount = columnWidths[columnIndex] - formattedLineLengths[lineIndex];
+
+        return line + ' '.repeat(padCount);
+      }).toJS().join('  | ')
     )).map(contentRow => `| ${contentRow}  |`);
 
     const separator = '|' + columnWidths.map(columnWidth => '-'.repeat(columnWidth + 3)).join('+') + '|';
