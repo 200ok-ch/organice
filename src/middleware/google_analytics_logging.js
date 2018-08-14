896a99afb5f5c77267acf29a4bab936b77c1acc8
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-08-13 19:15:50 -0700

    Don't send some fields to GA

--- src/middleware/google_analytics_logging.js
+++ src/middleware/google_analytics_logging.js
@@ -1,7 +1,20 @@
 /* global ga */
 
 export default store => next => action => {
-  const { type, ...payload } = action;
+  let { type, ...payload } = action;
+
+  const eventFieldsToRedact = {
+    'SET_CURRENT_FILE_BROWSER_DIRECTORY_LISTING': ['directoryListing'],
+    'AUTHENTICATE': ['accessToken'],
+    'DISPLAY_FILE': ['contents'],
+  };
+
+  if (!!eventFieldsToRedact[type]) {
+    eventFieldsToRedact[type].forEach(field => {
+      payload = {...payload, [field]: '--REDACTED--'};
+    });
+  }
+
   ga('send', 'event', 'action', type, JSON.stringify(payload));
 
   return next(action);
