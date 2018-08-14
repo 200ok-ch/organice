59a7609b41fc459f65349833d449f852f815a3e2
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-08-13 19:35:04 -0700

    Support redux devtools extension

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import googleAnalyticsLogger from './middleware/google_analytics_logging';
 import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default initialState => (
-  createStore(rootReducer, initialState, applyMiddleware(thunk, googleAnalyticsLogger))
+  createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk, googleAnalyticsLogger)))
 );
