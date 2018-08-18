6f4255920aac67e07ae85c348eb38c9ee0643ee7
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-08-18 09:47:21 -0700

    Implement template substitution

--- src/components/OrgFile/components/CaptureModal/index.js
+++ src/components/OrgFile/components/CaptureModal/index.js
@@ -5,6 +5,7 @@ import './CaptureModal.css';
 import ActionButton from '../ActionDrawer/components/ActionButton/';
 
 import _ from 'lodash';
+import moment from 'moment';
 
 export default class CaptureModal extends PureComponent {
   constructor(props) {
@@ -12,9 +13,22 @@ export default class CaptureModal extends PureComponent {
 
     _.bindAll(this, ['handleCaptureClick', 'handleTextareaChange']);
 
+    const [substitutedTemplate, initialCursorIndex] = this.substituteTemplateVariables(props.template.get('template'));
+
     this.state = {
-      textareaValue: '',
+      textareaValue: substitutedTemplate,
+      initialCursorIndex,
     };
+  }
+
+  componentDidMount() {
+    const { initialCursorIndex } = this.state;
+
+    this.textarea.focus();
+    if (initialCursorIndex !== null) {
+      this.textarea.selectionStart = initialCursorIndex;
+      this.textarea.selectionEnd = initialCursorIndex;
+    }
   }
 
   handleCaptureClick() {
@@ -26,6 +40,29 @@ export default class CaptureModal extends PureComponent {
 
   handleTextareaChange(event) {
     this.setState({ textareaValue: event.target.value });
+  }
+
+  substituteTemplateVariables(templateString) {
+    if (!templateString) {
+      return ['', null];
+    }
+
+    const substitutions = {
+      '%t' : `<${moment().format('YYYY-MM-DD ddd')}>`,
+      '%T' : `<${moment().format('YYYY-MM-DD ddd HH:mm')}>`,
+      '%u' : `[${moment().format('YYYY-MM-DD ddd')}]`,
+      '%U' : `[${moment().format('YYYY-MM-DD ddd HH:mm')}]`,
+    };
+
+    let substitutedString = templateString;
+    _.entries(substitutions).forEach(([formatString, value]) => (
+      substitutedString = substitutedString.replace(RegExp(formatString, 'g'), value)
+    ));
+
+    const cursorIndex = substitutedString.includes('%?') ? substitutedString.indexOf('%?') : null;
+    substitutedString = substitutedString.replace(/\%\?/, '');
+
+    return [substitutedString, cursorIndex];
   }
 
   render() {
@@ -49,9 +86,9 @@ export default class CaptureModal extends PureComponent {
 
         <textarea className="textarea capture-modal-textarea"
                   rows="4"
-                  autoFocus
                   value={textareaValue}
-                  onChange={this.handleTextareaChange} />
+                  onChange={this.handleTextareaChange}
+                  ref={textarea => this.textarea = textarea} />
 
         <div className="capture-modal-button-container">
           <button className="btn capture-modal-button" onClick={this.handleCaptureClick}>
