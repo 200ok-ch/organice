32d93e3d9a0d9ead832688703dd42e32bdda0a33
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-08-21 10:13:16 -0700

    Animate checkbox color transitions

--- src/components/UI/Checkbox/index.js
+++ src/components/UI/Checkbox/index.js
@@ -1,25 +1,40 @@
 import React, { PureComponent } from 'react';
+import { Motion, spring } from 'react-motion';
+
+import { interpolateColors, rgbaObject, rgbaString } from '../../../lib/color';
 
 import './Checkbox.css';
-
-import classNames from 'classnames';
 
 export default class Checkbox extends PureComponent {
   render() {
     const { state, onClick } = this.props;
 
-    const className = classNames('checkbox', {
-      'checkbox--checked': state === 'checked',
-      'checkbox--partial': state === 'partial',
-    });
+    const uncheckedColor = rgbaObject(255, 255, 255, 1);
+    const checkedColor = rgbaObject(94, 52, 140, 1);
+
+    const checkboxStyle = {
+      colorFactor: spring({
+        'checked': 1,
+        'partial': 1,
+        'unchecked': 0,
+      }[state], { stiffness: 300 })
+    };
 
     return (
-      <div className={className} onClick={onClick}>
-        <div className="checkbox__inner-container">
-          {state === 'checked' && <i className="fas fa-check" />}
-          {state === 'partial' && <i className="fas fa-minus" />}
-        </div>
-      </div>
+      <Motion style={checkboxStyle}>
+        {style => {
+          const backgroundColor = rgbaString(interpolateColors(uncheckedColor, checkedColor, style.colorFactor));
+
+          return (
+            <div className="checkbox" onClick={onClick} style={{backgroundColor}}>
+              <div className="checkbox__inner-container">
+                {state === 'checked' && <i className="fas fa-check" />}
+                {state === 'partial' && <i className="fas fa-minus" />}
+              </div>
+            </div>
+          );
+        }}
+      </Motion>
     );
   }
 }
