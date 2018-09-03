8d3fd421f847612bb09c4d63197d75079e8099b8
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-09-03 11:52:38 -0700

    Update design of table action drawer

--- src/components/OrgFile/components/AttributedString/components/TablePart/TableActionDrawer/index.js
+++ src/components/OrgFile/components/AttributedString/components/TablePart/TableActionDrawer/index.js
@@ -15,36 +15,29 @@ export default class TableActionDrawer extends PureComponent {
     } = this.props;
 
     return (
-      <div>
-        <div className="table-action-drawer-row">
-          <i className="fas fa-pencil-alt fa-lg" onClick={onEnterTableEditMode} />
-
-          <span className="table-action-drawer__separator" />
-
-          <div className="table-action-drawer__sub-icon-container" onClick={onAddNewTableRow}>
-            <i className="fas fa-plus fa-lg table-action-drawer__main-icon" />
-            <i className="fas fa-columns fa-sm table-action-drawer__sub-icon table-action-drawer__sub-icon--rotated" />
-          </div>
-
-          <span className="table-action-drawer__separator" />
-
-          <div className="table-action-drawer__sub-icon-container" onClick={onRemoveTableRow}>
-            <i className="fas fa-times fa-lg table-action-drawer__main-icon" />
-            <i className="fas fa-columns fa-sm table-action-drawer__sub-icon table-action-drawer__sub-icon--rotated" />
-          </div>
+      <div className="table-action-drawer-container">
+        <div className=" table-action-drawer__edit-icon-container" onClick={onEnterTableEditMode}>
+          <i className="fas fa-pencil-alt fa-lg" />
         </div>
-        <div className="table-action-drawer-row table-action-drawer-row--lower">
-          <div className="table-action-drawer__sub-icon-container" onClick={onAddNewTableColumn}>
-            <i className="fas fa-plus fa-lg table-action-drawer__main-icon" />
-            <i className="fas fa-columns fa-sm table-action-drawer__sub-icon" />
-          </div>
 
-          <span className="table-action-drawer__separator" />
+        <div className="table-action-drawer__sub-icon-container" onClick={onAddNewTableRow}>
+          <i className="fas fa-plus fa-lg table-action-drawer__main-icon" />
+          <i className="fas fa-columns fa-sm table-action-drawer__sub-icon table-action-drawer__sub-icon--rotated" />
+        </div>
 
-          <div className="table-action-drawer__sub-icon-container" onClick={onRemoveTableColumn}>
-            <i className="fas fa-times fa-lg table-action-drawer__main-icon" />
-            <i className="fas fa-columns fa-sm table-action-drawer__sub-icon" />
-          </div>
+        <div className="table-action-drawer__sub-icon-container" onClick={onRemoveTableRow}>
+          <i className="fas fa-times fa-lg table-action-drawer__main-icon" />
+          <i className="fas fa-columns fa-sm table-action-drawer__sub-icon table-action-drawer__sub-icon--rotated" />
+        </div>
+
+        <div className="table-action-drawer__sub-icon-container" onClick={onAddNewTableColumn}>
+          <i className="fas fa-plus fa-lg table-action-drawer__main-icon" />
+          <i className="fas fa-columns fa-sm table-action-drawer__sub-icon" />
+        </div>
+
+        <div className="table-action-drawer__sub-icon-container" onClick={onRemoveTableColumn}>
+          <i className="fas fa-times fa-lg table-action-drawer__main-icon" />
+          <i className="fas fa-columns fa-sm table-action-drawer__sub-icon" />
         </div>
       </div>
     );
