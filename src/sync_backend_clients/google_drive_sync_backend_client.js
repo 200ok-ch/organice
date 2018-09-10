cd9e17e98d0b0b6fbdaf28a8bcd840d273a46569
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-09-10 09:53:10 -0700

    Mock all functions in GDrive sync backend client

--- src/sync_backend_clients/google_drive_sync_backend_client.js
+++ src/sync_backend_clients/google_drive_sync_backend_client.js
@@ -1,11 +1,6 @@
 import { fromJS } from 'immutable';
 
-// TODO: remove this accessToken parameter.
-export default accessToken => {
-  // TODO: implement this.
-  const Dropbox = '';
-  const dropboxClient = new Dropbox({ accessToken });
-
+export default () => {
   const transformDirectoryListing = listing => (
     fromJS(listing.map(entry => ({
       id: entry.id,
@@ -17,49 +12,67 @@ export default accessToken => {
 
   const getDirectoryListing = path => (
     new Promise((resolve, reject) => {
-      dropboxClient.filesListFolder({ path }).then(response => (
-        resolve({
-          listing: transformDirectoryListing(response.entries),
-          hasMore: response.has_more,
-          cursor: response.cursor,
-        })
-      )).catch(reject);
+      resolve({
+        listing: fromJS([
+          {
+            id: 1,
+            name: 'test file 1',
+            isDirectory: false,
+            path: '/org/test file 1'
+          },
+          {
+            id: 2,
+            name: 'test dir 1',
+            isDirectory: true,
+            path: '/org/test dir 1'
+          },
+          {
+            id: 3,
+            name: 'test file 2',
+            isDirectory: false,
+            path: '/org/test file 2'
+          },
+        ]),
+        hasMore: true,
+        cursor: null,
+      });
     })
   );
 
   const getMoreDirectoryListing = cursor => (
     new Promise((resolve, reject) => (
-      dropboxClient.filesListFolderContinue({ cursor }).then(response => (
-        resolve({
-          listing: transformDirectoryListing(response.entries),
-          hasMore: response.has_more,
-          cursor: response.cursor,
-        })
-      ))
+      resolve({
+        listing: fromJS([
+          {
+            id: 4,
+            name: 'test file 3',
+            isDirectory: false,
+            path: '/org/test file 3'
+          },
+          {
+            id: 5,
+            name: 'test dir 2',
+            isDirectory: true,
+            path: '/org/test dir 2'
+          },
+        ]),
+        hasMore: true,
+        cursor: null,
+      })
     ))
   );
 
   const uploadFile = (path, contents) => (
     new Promise((resolve, reject) => (
-      dropboxClient.filesUpload({
-        path, contents,
-        mode: {
-          '.tag': 'overwrite',
-        },
-        autorename: true,
-      }).then(resolve).catch(reject)
+      resolve()
     ))
   );
 
   const getFileContentsAndMetadata = path => (
     new Promise((resolve, reject) => (
-      dropboxClient.filesDownload({ path }).then(response => {
-        const reader = new FileReader();
-        reader.addEventListener('loadend', () => resolve({
-          contents: reader.result,
-          lastModifiedAt: response.server_modified,
-        }));
-        reader.readAsText(response.fileBlob);
+      resolve({
+        contents: '* test file contents',
+        lastModifiedAt: 'some date',
       })
     ))
   );
@@ -72,9 +85,7 @@ export default accessToken => {
 
   const deleteFile = path => (
     new Promise((resolve, reject) => (
-      dropboxClient.filesDelete({ path }).then(resolve).catch(error => (
-        reject(error.error.error['.tag'] === 'path_lookup', error)
-      ))
+      resolve()
     ))
   );
 
