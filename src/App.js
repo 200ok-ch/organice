c5c5bdace8f3acf02318841d3862abee1ec2cee4
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-09-11 19:06:38 -0700

    Fix async load of GDrive SDK

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ export default class App extends PureComponent {
     if (window.gapi) {
       checkForGoogleDriveLogin();
     } else {
-      window.handleGoogleDriveClick = checkForGoogleDriveLogin();
+      window.handleGoogleDriveClick = checkForGoogleDriveLogin;
     }
   }
 
