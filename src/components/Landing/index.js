5c1369dec8ef3c0165bd50e3cdfd562d9960aee8
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-09-13 22:04:21 -0700

    Update references to Dropbox to include GDrive

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -10,7 +10,7 @@ export default class Landing extends PureComponent {
         <h1 className="landing-app-name">org-web</h1>
         <h2 className="landing-tagline">Edit your org files online.</h2>
         <h2 className="landing-tagline">Optimized for mobile.</h2>
-        <h2 className="landing-tagline">Syncs with Dropbox.</h2>
+        <h2 className="landing-tagline">Syncs with Dropbox<br />and Google Drive.</h2>
 
         <Link to="/sample">
           <div className="btn landing-button view-sample-button">View sample</div>
