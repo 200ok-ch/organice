ddead0534c3c1a571612e803f07f299e11827988
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-10-04 12:13:59 -0700

    Add skeleton of new SlideUp component

---
+++ src/components/UI/SlideUp/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+import './stylesheet.css';
+
+export default class SlideUp extends PureComponent {
+  render() {
+    return <div>Slide up!</div>;
+  }
+}
