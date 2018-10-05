82d1b7d9301ba2e7f15f5c8180aab2fb30746269
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-10-05 11:18:00 -0700

    Display selected timeframe and implement next/previous chevrons

--- src/components/OrgFile/components/AgendaModal/index.js
+++ src/components/OrgFile/components/AgendaModal/index.js
@@ -12,15 +12,77 @@ export default class AgendaModal extends PureComponent {
   constructor(props) {
     super(props);
 
-    _.bindAll(this, ['handleTimeframeTypeChange']);
+    _.bindAll(this, [
+      'handleTimeframeTypeChange',
+      'handleNextDateClick',
+      'handlePreviousDateClick',
+    ]);
 
     this.state = {
+      date: moment(),
       timeframeType: 'Week',
     };
   }
 
   handleTimeframeTypeChange(timeframeType) {
     this.setState({ timeframeType });
+  }
+
+  handleNextDateClick() {
+    console.log('next');
+    const { date, timeframeType } = this.state;
+
+    switch (timeframeType) {
+      case 'Day':
+        this.setState({ date: date.clone().add(1, 'day') });
+        break;
+      case 'Week':
+        this.setState({ date: date.clone().add(1, 'week') });
+        break;
+      case 'Month':
+        this.setState({ date: date.clone().add(1, 'month') });
+        break;
+      default:
+        return '';
+    }
+  }
+
+  handlePreviousDateClick() {
+    console.log('previous');
+    const { date, timeframeType } = this.state;
+
+    switch (timeframeType) {
+      case 'Day':
+        this.setState({ date: date.clone().subtract(1, 'day') });
+        break;
+      case 'Week':
+        this.setState({ date: date.clone().subtract(1, 'week') });
+        break;
+      case 'Month':
+        this.setState({ date: date.clone().subtract(1, 'month') });
+        break;
+      default:
+        return '';
+    }
+  }
+
+  calculateTimeframeHeader() {
+    const { date, timeframeType } = this.state;
+
+    switch (timeframeType) {
+      case 'Day':
+        return date.format('MMMM Do');
+      case 'Week':
+        const weekStart = date.clone().startOf('week');
+        const weekEnd = weekStart.clone().add(1, 'week');
+        return `${weekStart.format('MMM Do')} - ${weekEnd.format('MMM Do')} (W${weekStart.format(
+          'w'
+        )})`;
+      case 'Month':
+        return date.format('MMMM');
+      default:
+        return '';
+    }
   }
 
   render() {
@@ -30,7 +92,6 @@ export default class AgendaModal extends PureComponent {
     return (
       <SlideUp shouldIncludeCloseButton onClose={onClose}>
         <h2 className="agenda__title">Agenda</h2>
-
         <div className="agenda__tab-container">
           <TabButtons
             buttons={['Day', 'Week', 'Month']}
@@ -39,13 +100,30 @@ export default class AgendaModal extends PureComponent {
             useEqualWidthTabs
           />
         </div>
-
         <div className="agenda__timeframe-header-container">
-          <i className="fas fa-chevron-left fa-lg" />
-          <div className="agenda__timeframe-header">Sep 30 - Oct 6 (W40)</div>
-          <i className="fas fa-chevron-right fa-lg" />
+          <i className="fas fa-chevron-left fa-lg" onClick={this.handlePreviousDateClick} />
+          <div className="agenda__timeframe-header">{this.calculateTimeframeHeader()}</div>
+          <i className="fas fa-chevron-right fa-lg" onClick={this.handleNextDateClick} />
         </div>
-
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         <br />
       </SlideUp>
     );
