652b98c6cbc17294796bfa45aa4d3902d50b910a
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2018-10-30 11:55:35 -0700

    Convert property list component to use hooks

--- src/components/OrgFile/components/HeaderContent/components/PropertyListItems/index.js
+++ src/components/OrgFile/components/HeaderContent/components/PropertyListItems/index.js
@@ -1,69 +1,48 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import './stylesheet.css';
 
 import AttributedString from '../../../AttributedString';
 
-import _ from 'lodash';
+export default ({ propertyListItems, shouldDisableActions, onTimestampClick, onEdit }) => {
+  const [isDrawerCollapsed, setIsDrawerCollapsed] = useState(true);
 
-export default class PropertyListItems extends PureComponent {
-  constructor(props) {
-    super(props);
+  const handleCollapseToggle = () => setIsDrawerCollapsed(!isDrawerCollapsed);
 
-    _.bindAll(this, ['handleCollapseToggle']);
-
-    this.state = {
-      isDrawerCollapsed: true,
-    };
-  }
-
-  handleCollapseToggle() {
-    this.setState({ isDrawerCollapsed: !this.state.isDrawerCollapsed });
-  }
-
-  render() {
-    const { propertyListItems, shouldDisableActions, onTimestampClick, onEdit } = this.props;
-    const { isDrawerCollapsed } = this.state;
-
-    if (propertyListItems.size === 0) {
-      return null;
-    }
-
-    return (
-      <div className="property-list-container">
-        <div className="property-list__property" onClick={this.handleCollapseToggle}>
-          :PROPERTIES:
-          {isDrawerCollapsed ? '...' : ''}
-        </div>
-
-        {!isDrawerCollapsed && (
-          <Fragment>
-            {propertyListItems.map(propertyListItem => (
-              <div className="property-list__item-container" key={propertyListItem.get('id')}>
-                <div
-                  className="property-list__property"
-                  onClick={shouldDisableActions ? null : onEdit}
-                >
-                  :{propertyListItem.get('property')}:
-                </div>
-                <div className="property-list__value">
-                  <AttributedString
-                    parts={propertyListItem.get('value') || []}
-                    subPartDataAndHandlers={{
-                      onTimestampClick,
-                      shouldDisableActions,
-                    }}
-                  />
-                </div>
-              </div>
-            ))}
-
-            <div className="property-list__property" onClick={onEdit}>
-              :END:
-            </div>
-          </Fragment>
-        )}
+  return propertyListItems.size === 0 ? null : (
+    <div className="property-list-container">
+      <div className="property-list__property" onClick={handleCollapseToggle}>
+        :PROPERTIES:
+        {isDrawerCollapsed ? '...' : ''}
       </div>
-    );
-  }
-}
+
+      {!isDrawerCollapsed && (
+        <Fragment>
+          {propertyListItems.map(propertyListItem => (
+            <div className="property-list__item-container" key={propertyListItem.get('id')}>
+              <div
+                className="property-list__property"
+                onClick={shouldDisableActions ? null : onEdit}
+              >
+                :{propertyListItem.get('property')}:
+              </div>
+              <div className="property-list__value">
+                <AttributedString
+                  parts={propertyListItem.get('value') || []}
+                  subPartDataAndHandlers={{
+                    onTimestampClick,
+                    shouldDisableActions,
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+
+          <div className="property-list__property" onClick={onEdit}>
+            :END:
+          </div>
+        </Fragment>
+      )}
+    </div>
+  );
+};
