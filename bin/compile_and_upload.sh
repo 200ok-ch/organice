e5f3420a0fb50fc9f1db2ba27dedb078fd773533
Alain M. Lafon <alain@200ok.ch>
2019-08-26 09:22:18 +0200

    feat: CircleCI workflow to use CI for CD

---
+++ bin/compile_and_upload.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# TODO: Check for existence of $HOST, $USER, $PASSWD
+
+set -e
+
+yarn install
+yarn run build
+cd build
+lftp "$HOST" <<END_SCRIPT
+user $USER $PASSWD
+mirror -R ./
+quit
+END_SCRIPT
+exit 0
