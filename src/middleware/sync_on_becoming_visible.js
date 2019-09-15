809a909ee80453fbeae3a894e2b95bec839c6626
Alain M. Lafon <alain@200ok.ch>
2019-09-15 20:47:05 +0200

    feat: 'Sync on application becoming visible' works on Safari

--- src/middleware/sync_on_becoming_visible.js
+++ src/middleware/sync_on_becoming_visible.js
@@ -6,14 +6,34 @@ const dispatchSync = store => store.dispatch(sync({ shouldSuppressMessages: true
 // The 'visibilitychange' event might get triggered in some browsers many times for one 'real'
 // event of the browser becoming visible to the user. Debouncing through lodash ensures that
 // it is called at maximum once every second.
-var debouncedDispatchSync = debounce(dispatchSync, 1000);
+const debouncedDispatchSync = debounce(dispatchSync, 1000);
+
+// Dealing with vendor prefixes
+function getHiddenProp() {
+  const prefixes = ['webkit', 'moz', 'ms', 'o'];
+
+  // if 'hidden' is natively supported just return it
+  if ('hidden' in document) return 'hidden';
+
+  // otherwise loop over all the known prefixes until we find one
+  for (let i = 0; i < prefixes.length; i++) {
+    if (prefixes[i] + 'Hidden' in document) return prefixes[i] + 'Hidden';
+  }
+
+  // otherwise it's not supported
+  return null;
+}
 
 export default store => next => action => {
-  window.addEventListener('visibilitychange', () => {
-    if (store.getState().base.get('shouldSyncOnBecomingVisibile')) {
-      debouncedDispatchSync(store);
-    }
-  });
+  let visProp = getHiddenProp();
+  if (visProp) {
+    const evtname = visProp.replace(/[H|h]idden/, '') + 'visibilitychange';
+    document.addEventListener(evtname, () => {
+      if (store.getState().base.get('shouldSyncOnBecomingVisibile')) {
+        debouncedDispatchSync(store);
+      }
+    });
+  }
 
   return next(action);
 };
