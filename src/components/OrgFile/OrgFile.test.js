16f563bd4ccee208c761575e2533836a49564a54
Alain M. Lafon <alain@200ok.ch>
2019-09-22 14:52:34 +0200

    test: Test Agenda view and interaction

--- src/components/OrgFile/OrgFile.test.js
+++ src/components/OrgFile/OrgFile.test.js
@@ -125,7 +125,7 @@ describe('Unit Tests for org file', () => {
   });
 });
 
-describe('Rendering an org file', () => {
+describe('Render all views', () => {
   jest.mock('react-hotkeys', () => {
     const React = require('react');
     const Fragment = React.Fragment;
@@ -138,7 +138,8 @@ describe('Rendering an org file', () => {
   const testOrgFile = `
 * Top level header
 ** A nested header
-** TODO A todo item
+** TODO A scheduled todo item
+   SCHEDULED: <2019-09-19 Thu>
 * Another top level header
 Some description content
 * A header with tags                                              :tag1:tag2:
@@ -170,67 +171,105 @@ Some description content
     store.dispatch(displayFile('fixtureTestFile.org', testOrgFile));
   });
 
-  test('<OrgFile /> renders an org file', () => {
-    const { container, getAllByText } = render(
-      <MemoryRouter keyLength={0}>
-        <Provider store={store}>
-          <OrgFile path="fixtureTestFile.org" />
-        </Provider>
-      </MemoryRouter>
-    );
+  describe('Actions within an Org file', () => {
+    test('Can select a header in an org file', () => {
+      const { getByText, queryByText } = render(
+        <MemoryRouter keyLength={0}>
+          <Provider store={store}>
+            <OrgFile path="fixtureTestFile.org" />
+          </Provider>
+        </MemoryRouter>
+      );
 
-    expect(getAllByText(/\*/)).toHaveLength(4);
-    expect(container).toMatchSnapshot();
+      expect(queryByText('Scheduled')).toBeFalsy();
+      expect(queryByText('Deadline')).toBeFalsy();
+
+      fireEvent.click(getByText('Top level header'));
+
+      expect(queryByText('Scheduled')).toBeTruthy();
+      expect(queryByText('Deadline')).toBeTruthy();
+    });
+
+    // Org Mode has keywords as workflow states and can cycle through
+    // them: https://orgmode.org/manual/Workflow-states.html
+    // In organice, we can cycle through them by swiping or by clicking
+    // (if enabled). This test checks for the latter.
+    test('Can advance todo state for selected header in an org file', () => {
+      const { queryByText } = render(
+        <MemoryRouter keyLength={0}>
+          <Provider store={store}>
+            <OrgFile path="fixtureTestFile.org" />
+          </Provider>
+        </MemoryRouter>
+      );
+
+      // In the very beginning, the TODO is hidden, because the file
+      // starts folded down to the top level
+      expect(queryByText('TODO')).toBeFalsy();
+
+      // Cycle the top header (which will make the next headers
+      // [including the TODO] visible
+      fireEvent.click(queryByText('Top level header'));
+
+      // In the beginning, the TODO is not DONE
+      expect(queryByText('TODO')).toBeTruthy();
+      expect(queryByText('DONE')).toBeFalsy();
+
+      // Toggle the TODO
+      fireEvent.click(queryByText('TODO'));
+
+      // Then the TODO is DONE
+      expect(queryByText('TODO')).toBeFalsy();
+      expect(queryByText('DONE')).toBeTruthy();
+    });
   });
 
-  test('Can select a header in an org file', () => {
-    const { getByText, queryByText } = render(
-      <MemoryRouter keyLength={0}>
-        <Provider store={store}>
-          <OrgFile path="fixtureTestFile.org" />
-        </Provider>
-      </MemoryRouter>
-    );
+  describe('Renders everything starting from an Org file', () => {
+    let container, getAllByText, getByTitle, getByAltText, queryByText;
+    beforeEach(() => {
+      let res = render(
+        <MemoryRouter keyLength={0}>
+          <Provider store={store}>
+            <OrgFile path="fixtureTestFile.org" />
+          </Provider>
+        </MemoryRouter>
+      );
 
-    expect(queryByText('Scheduled')).toBeFalsy();
-    expect(queryByText('Deadline')).toBeFalsy();
+      container = res.container;
+      getAllByText = res.getAllByText;
+      getByAltText = res.getByAltText;
+      getByTitle = res.getByTitle;
+      queryByText = res.queryByText;
+    });
 
-    fireEvent.click(getByText('Top level header'));
+    test('renders an Org file', () => {
+      expect(getAllByText(/\*/)).toHaveLength(4);
+      expect(container).toMatchSnapshot();
+    });
 
-    expect(queryByText('Scheduled')).toBeTruthy();
-    expect(queryByText('Deadline')).toBeTruthy();
-  });
+    describe('Agenda', () => {
+      test('renders Agenda for an Org file', () => {
+        // Agenda is not visible by default
+        expect(queryByText('Agenda')).toBeFalsy();
+        expect(queryByText('Day')).toBeFalsy();
+        expect(queryByText('Month')).toBeFalsy();
+        expect(queryByText('A scheduled todo item')).toBeFalsy();
 
-  // Org Mode has keywords as workflow states and can cycle through
-  // them: https://orgmode.org/manual/Workflow-states.html
-  // In organice, we can cycle through them by swiping or by clicking
-  // (if enabled). This test checks for the latter.
-  test('Can advance todo state for selected header in an org file', () => {
-    const { queryByText } = render(
-      <MemoryRouter keyLength={0}>
-        <Provider store={store}>
-          <OrgFile path="fixtureTestFile.org" />
-        </Provider>
-      </MemoryRouter>
-    );
+        fireEvent.click(getByTitle('Show agenda'));
 
-    // In the very beginning, the TODO is hidden, because the file
-    // starts folded down to the top level
-    expect(queryByText('TODO')).toBeFalsy();
+        expect(queryByText('Agenda')).toBeTruthy();
+        expect(queryByText('Day')).toBeTruthy();
+        expect(queryByText('Month')).toBeTruthy();
+        expect(queryByText('A scheduled todo item')).toBeTruthy();
+      });
 
-    // Cycle the top header (which will make the next headers
-    // [including the TODO] visible
-    fireEvent.click(queryByText('Top level header'));
-
-    // In the beginning, the TODO is not DONE
-    expect(queryByText('TODO')).toBeTruthy();
-    expect(queryByText('DONE')).toBeFalsy();
-
-    // Toggle the TODO
-    fireEvent.click(queryByText('TODO'));
-
-    // Then the TODO is DONE
-    expect(queryByText('TODO')).toBeFalsy();
-    expect(queryByText('DONE')).toBeTruthy();
+      test('Clicking a TODO within the agenda highlights it in the main view', () => {
+        fireEvent.click(getByTitle('Show agenda'));
+        fireEvent.click(queryByText('A scheduled todo item'));
+        expect(queryByText('Agenda')).toBeFalsy();
+        expect(queryByText('A scheduled todo item')).toBeTruthy();
+        expect(queryByText('Scheduled')).toBeTruthy();
+      });
+    });
   });
 });
