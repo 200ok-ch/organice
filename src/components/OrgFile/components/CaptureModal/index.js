79c26b4a447cf3af16993407a07ecc329e2cb125
Alain M. Lafon <alain@200ok.ch>
2019-10-02 23:24:18 +0200

    fix: iOS 13 regression: On capture, focus and show input field
         Closes: #46

--- src/components/OrgFile/components/CaptureModal/index.js
+++ src/components/OrgFile/components/CaptureModal/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect, useRef, useMemo } from 'react';
+import Bowser from 'bowser';
 
 import './stylesheet.css';
 
@@ -23,17 +24,64 @@ export default ({ template, onCapture, headers, onClose }) => {
   const [textareaValue, setTextareaValue] = useState(substitutedTemplate);
   const [shouldPrepend, setShouldPrepend] = useState(template.get('shouldPrepend'));
 
+  const browser = Bowser.getParser(window.navigator.userAgent);
+
+  const isNewMobileSafari = browser.satisfies({
+    mobile: {
+      safari: '>=13',
+    },
+  });
+
+  // INFO: Mobile Safari does _not_ like it when the focus is set
+  // without an explicit user interaction (which is the case here,
+  // because the user interaction is on a button which in turn opens a
+  // textarea which should have the focus). It will open the software
+  // keyboard, but the capture template will stay on the bottom of the
+  // view, so it will be hidden by the keyboard. The user would have
+  // to manually scroll down. On iOS 12, it worked without this
+  // workaround.
+  // The following `min-height` values are measured for an iPhone Xs.
+  // TODO: Measure the values for other iPhone form factors and iPads.
+  const getMinHeight = () => {
+    // Only Mobile Safari needs the shenannigans for moving the input
+    // above the software keyboard.
+    if (isNewMobileSafari) {
+      // Within PWA
+      if ('standalone' in window.navigator && window.navigator.standalone) {
+        // Landscape mode
+        if ([90, -90].includes(window.orientation)) {
+          return '9em';
+        }
+        // Portrait Mode
+        else {
+          return '23em';
+        }
+      }
+      // Not within PWA, but standard Mobile Safari
+      else {
+        // Landscape mode
+        if ([90, -90].includes(window.orientation)) {
+          return '9em';
+        }
+        // Portrait Mode
+        else {
+          return '18em';
+        }
+      }
+    } else {
+      return 'auto';
+    }
+  };
+
   const textarea = useRef(null);
 
   useEffect(() => {
     if (textarea.current) {
       textarea.current.focus();
-      if (initialCursorIndex !== null) {
-        textarea.current.selectionStart = initialCursorIndex;
-        textarea.current.selectionEnd = initialCursorIndex;
-      }
     }
-  }, [textarea, initialCursorIndex]);
+    // Set Cursor position to the %? part of the template.
+    textarea.current.setSelectionRange(initialCursorIndex, initialCursorIndex);
+  }, [textarea]);
 
   const handleCaptureClick = () => onCapture(template.get('id'), textareaValue, shouldPrepend);
 
@@ -64,9 +112,9 @@ export default ({ template, onCapture, headers, onClose }) => {
             rows="4"
             value={textareaValue}
             onChange={handleTextareaChange}
+            autoFocus={true}
             ref={textarea}
           />
-
           <div className="capture-modal-button-container">
             <div className="capture-modal-prepend-container">
               <span className="capture-modal-prepend-label">Prepend:</span>
@@ -77,6 +125,9 @@ export default ({ template, onCapture, headers, onClose }) => {
               Capture
             </button>
           </div>
+          {/* Add padding to move the above textarea above the fold.
+          More documentation, see getMinHeight(). */}
+          {isNewMobileSafari && <div style={{ minHeight: getMinHeight() }} />}
         </Fragment>
       ) : (
         <div className="capture-modal-error-message">
