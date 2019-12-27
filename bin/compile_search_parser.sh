61f5bd981a57e20b6c19e29b73b41afb57fc34f3
Alain M. Lafon <alain@200ok.ch>
2019-12-27 15:26:02 +0100

    fix: Ignore eslint warnings for generated parser code

--- bin/compile_search_parser.sh
+++ bin/compile_search_parser.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 npx pegjs -o src/lib/headline_filter_parser.{js,grammar.pegjs}
+
+# The generated code of pegjs throws eslint warnings. Since it's not
+# code that we're writing and we want to keep eslint warnings at 0,
+# we're telling eslint to ignore the generated file.
+PARSER_FILE=src/lib/headline_filter_parser.js
+# Prepend the generated parser code with a `eslint-disable` comment.
+echo "/* eslint-disable */"|cat - $PARSER_FILE > /tmp/out && mv /tmp/out $PARSER_FILE
