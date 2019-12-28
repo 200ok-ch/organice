e36ab6316929852d027d5426b663aa8c7ef2e76e
Jakob Schöttl <jschoett@gmail.com>
2019-12-28 10:13:03 +0100

    Bugfix

--- src/reducers/org.js
+++ src/reducers/org.js
@@ -247,8 +247,10 @@ const addHeader = (state, action) => {
 
   const subheaders = subheadersOfHeaderWithId(headers, action.headerId);
 
+  const todoKeyword = header.getIn(['titleLine', 'todoKeyword']);
+
   const newHeader = newHeaderWithTitle(
-    header.getIn(['titleLine', 'todoKeyword']) + ' ',
+    todoKeyword ? todoKeyword + ' ' : '',
     header.get('nestingLevel'),
     state.get('todoKeywordSets')
   );
