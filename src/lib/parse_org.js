16ae16346635ce052770437fc6a6b5739d964e2a
Jakob Schöttl <jschoett@gmail.com>
2019-12-30 18:30:09 +0100

    Bugfix

--- src/lib/parse_org.js
+++ src/lib/parse_org.js
@@ -617,7 +617,7 @@ const parseLogbook = rawText => {
 export const parseDescriptionPrefixElements = (rawText, parsedTitle) => {
   const planningItemsParse = _parsePlanningItems(rawText);
   const planningItemsFromTitle = parsedTitle
-    .filter(x => x.get('type') === 'timestamp' && x.getIn(['timestamp', 'isActive']))
+    .filter(x => x.get('type') === 'timestamp' && x.getIn(['firstTimestamp', 'isActive']))
     .map(x => createTimestamp({ type: 'TIMESTAMP', timestamp: x.get('firstTimestamp') })); // ignore second timestamp
 
   const planningItems = planningItemsParse.planningItems.merge(planningItemsFromTitle);
@@ -706,7 +706,7 @@ export const newHeaderFromText = (rawText, todoKeywordSets) => {
     propertyListItems,
     strippedDescription,
     logBookEntries,
-  } = parseDescriptionPrefixElements(description, fromJS([])); // TODO cannot pass parsed title here, as it is parsed only in the next line...
+  } = parseDescriptionPrefixElements(description, fromJS([])); // TODO: cannot pass parsed title here, as it is parsed only in the next line...
 
   return newHeaderWithTitle(titleLine, 1, todoKeywordSets)
     .set('rawDescription', strippedDescription)
