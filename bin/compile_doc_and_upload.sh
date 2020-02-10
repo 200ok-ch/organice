038fc6a54a2939e232346850676b16b36db6b24a
Alain M. Lafon <alain@200ok.ch>
2020-02-10 08:57:15 +0100

    doc: Bring more structure to the canonical documentation

--- bin/compile_doc_and_upload.sh
+++ bin/compile_doc_and_upload.sh
@@ -2,13 +2,12 @@
 
 echo "#+SETUPFILE: https://fniessen.github.io/org-html-themes/setup/theme-readtheorg.setup" > documentation.org
 cat README.org | grep -v api.codeclimate | grep -v "https://organice.200ok.ch/documentation.html" >> documentation.org
+sed -i 's/# REPO_PLACEHOLDER/Code repository: https:\/\/github.com\/200ok-ch\/organice/' documentation.org
 cat WIKI.org >> documentation.org
 cat CONTRIBUTING.org >> documentation.org
 pandoc CODE_OF_CONDUCT.md -o CODE_OF_CONDUCT.org
 cat CODE_OF_CONDUCT.org >> documentation.org
 rm CODE_OF_CONDUCT.org
 emacs documentation.org -l ./doc/org2html/init.el --batch  --funcall org-html-export-to-html
-
-emacs --version
 
 lftp -e "put documentation.html; exit" -u $FTP_USER,$FTP_PASSWD $FTP_HOST
