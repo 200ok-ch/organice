580c98c81bbd442a09b36c2b3bf4df4926c79487
William Connolly <william.connolly@lithium.com>
2020-02-18 09:32:26 -0600

    Remove whitespace

--- bin/compile_search_parser.sh
+++ bin/compile_search_parser.sh
@@ -6,7 +6,5 @@ npx pegjs -o src/lib/headline_filter_parser.{js,grammar.pegjs}
 # code that we're writing and we want to keep eslint warnings at 0,
 # we're telling eslint to ignore the generated file.
 PARSER_FILE=src/lib/headline_filter_parser.js
-
 # Prepend the generated parser code with a `eslint-disable` comment.
 sed -i '1i \/* eslint-disable */' "$PARSER_FILE"
-
