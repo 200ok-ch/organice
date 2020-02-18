7f32d83c3531bfbeb1c145e9d220b8a0bb1a8c7e
William Connolly <william.connolly@lithium.com>
2020-02-18 09:31:54 -0600

    Revert compile_search_parser

--- bin/compile_search_parser.sh
+++ bin/compile_search_parser.sh
@@ -6,10 +6,7 @@ npx pegjs -o src/lib/headline_filter_parser.{js,grammar.pegjs}
 # code that we're writing and we want to keep eslint warnings at 0,
 # we're telling eslint to ignore the generated file.
 PARSER_FILE=src/lib/headline_filter_parser.js
-TMP_FILE=/tmp/headline_filter_parser.js
 
 # Prepend the generated parser code with a `eslint-disable` comment.
-echo '/* eslint-disable */' > "$TMP_FILE"
-cat "$PARSER_FILE" >> "$TMP_FILE"
-mv "$TMP_FILE" "$PARSER_FILE"
+sed -i '1i \/* eslint-disable */' "$PARSER_FILE"
 
