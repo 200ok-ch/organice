3776af07a55d769b069e45ec811480b99bc3eba9
Alain M. Lafon <alain@200ok.ch>
2020-06-08 18:29:21 +0200

    chore: Migrate misspelled `shouldSyncOnBecomingVisibile` config flag

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,7 +1,9 @@
 import migrateAccessTokenToDropboxAccessToken from './migrate_access_token_to_dropbox_access_token';
 import migrateStoreInDropboxToStoreInSyncBackend from './migrate_store_in_dropbox_to_store_in_sync_backend';
+import migrateSyncVisibleFlag from './migrate_sync_visible_flag';
 
 export default () => {
   migrateAccessTokenToDropboxAccessToken();
   migrateStoreInDropboxToStoreInSyncBackend();
+  migrateSyncVisibleFlag();
 };
---
+++ src/migrations/migrate_sync_visible_flag.js
@@ -0,0 +1,16 @@
+import { isLocalStorageAvailable } from '../util/settings_persister';
+
+// Fixes a typo in the config: `visibile` !== `visible`
+export default () => {
+  if (!isLocalStorageAvailable) {
+    return;
+  }
+
+  const shouldSyncOnBecomingVisible = localStorage.getItem('shouldSyncOnBecomingVisibile');
+  if (!shouldSyncOnBecomingVisible) {
+    return;
+  }
+
+  localStorage.setItem('shouldSyncOnBecomingVisibile', shouldSyncOnBecomingVisible);
+  localStorage.removeItem('shouldSyncOnBecomingVisibile');
+};
