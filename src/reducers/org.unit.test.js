5730908f477ccd28a8e6b09a33ca75a4cb678bd0
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2020-08-18 13:19:22 +0200

    Add tests for MOVE_SUBTREE_LEFT MOVE_SUBTREE_RIGHT
    org.js statement coverage increase: 50.2 -> 53.0

--- src/reducers/org.unit.test.js
+++ src/reducers/org.unit.test.js
@@ -277,4 +277,82 @@ describe('org reducer', () => {
       check_is_undoable(state, types.moveHeaderRight(nestedHeaderId));
     });
   });
+
+  describe('MOVE_SUBTREE_LEFT', () => {
+    let nestedHeaderId;
+    let state;
+    const testOrgFile = readFixture('nested_header');
+
+    beforeEach(() => {
+      state = readInitialState();
+      state.org.present = parseOrg(testOrgFile);
+      // The target is to move "A nested header" to the top level.
+
+      // "A nested header" the 2nd item but we count from 0 not 1.
+      nestedHeaderId = state.org.present.get('headers').get(1).get('id');
+    });
+
+    it('should handle MOVE_SUBTREE_LEFT', () => {
+      // Mapping the headers to their nesting level. This is how the
+      // initially parsed file should look like.
+      expect(extractTitlesAndNestings(state.org.present.get('headers'))).toEqual([
+        ['Top level header', 1],
+        ['A nested header', 2],
+        ['A deep nested header', 3],
+      ]);
+
+      const action = types.moveSubtreeLeft(nestedHeaderId);
+      const newState = reducer(state.org.present, action);
+
+      // "A nested header" is not at the top level.
+      expect(extractTitlesAndNestings(newState.get('headers'))).toEqual([
+        ['Top level header', 1],
+        ['A nested header', 1],
+        ['A deep nested header', 2],
+      ]);
+    });
+
+    it('is undoable', () => {
+      check_is_undoable(state, types.moveSubtreeLeft(nestedHeaderId));
+    });
+  });
+
+  describe('MOVE_SUBTREE_RIGHT', () => {
+    let nestedHeaderId;
+    let state;
+    const testOrgFile = readFixture('nested_header');
+
+    beforeEach(() => {
+      state = readInitialState();
+      state.org.present = parseOrg(testOrgFile);
+      // The target is to move "A nested header" to the top level.
+
+      // "A nested header" the 2nd item but we count from 0 not 1.
+      nestedHeaderId = state.org.present.get('headers').get(1).get('id');
+    });
+
+    it('should handle MOVE_SUBTREE_RIGHT', () => {
+      // Mapping the headers to their nesting level. This is how the
+      // initially parsed file should look like.
+      expect(extractTitlesAndNestings(state.org.present.get('headers'))).toEqual([
+        ['Top level header', 1],
+        ['A nested header', 2],
+        ['A deep nested header', 3],
+      ]);
+
+      const action = types.moveSubtreeRight(nestedHeaderId);
+      const newState = reducer(state.org.present, action);
+
+      // "A nested header" is not at the top level.
+      expect(extractTitlesAndNestings(newState.get('headers'))).toEqual([
+        ['Top level header', 1],
+        ['A nested header', 3],
+        ['A deep nested header', 4],
+      ]);
+    });
+
+    it('is undoable', () => {
+      check_is_undoable(state, types.moveSubtreeRight(nestedHeaderId));
+    });
+  });
 });
