2ad5dda8a30ffd22e2f1da7dcf85575ad84026be
Alain M. Lafon <alain@200ok.ch>
2020-10-18 19:12:23 +0200

    fix: `bin/compile_and_upload.sh` does not fail early

--- bin/compile_and_upload.sh
+++ bin/compile_and_upload.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Sanity check that all variables are set to upload to FTP
-[ -z ${FTP_HOST+x} ] && echo "$FTP_HOST needs to be set for uploading to FTP."; exit 1
-[ -z ${FTP_USER+x} ] && echo "$FTP_USER needs to be set for uploading to FTP."; exit 1
-[ -z ${FTP_PASSWD+x} ] && echo "$FTP_PASSWD needs to be set for uploading to FTP."; exit 1
+[ -z ${FTP_HOST+x} ] && (echo "$FTP_HOST needs to be set for uploading to FTP."; exit 1)
+[ -z ${FTP_USER+x} ] && (echo "$FTP_USER needs to be set for uploading to FTP."; exit 1)
+[ -z ${FTP_PASSWD+x} ] && (echo "$FTP_PASSWD needs to be set for uploading to FTP."; exit 1)
 
 # Configure available back-end API keys
 cp .env.sample .env
