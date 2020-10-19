80e2c2bbced0621eec8ba5ca741cf6ca3cd3a69a
Alain M. Lafon <alain@200ok.ch>
2020-10-19 20:45:04 +0200

    test: 'search' operates solely on subheaders if a header is focused

--- src/reducers/org.js
+++ src/reducers/org.js
@@ -1056,7 +1056,7 @@ export const setSearchFilterInformation = (state, action) => {
 
     // Only search subheaders if a header is focused
     const focusedHeaderId = state.get('focusedHeaderId');
-    if (focusedHeaderId === undefined || focusedHeaderId === null || context === 'refile') {
+    if (!focusedHeaderId || context === 'refile') {
       filteredHeaders = headers.filter(isMatch(searchFilterExpr));
     } else {
       const subheaders = subheadersOfHeaderWithId(headers, focusedHeaderId);
