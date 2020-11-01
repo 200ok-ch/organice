24eb5aba07dc73a2e0ff1e1da2a9cb53e7e86678
Alain M. Lafon <alain@200ok.ch>
2020-11-01 20:23:15 +0100

    test: Actually use and persist default settings

--- src/util/settings_persister.test.js
+++ src/util/settings_persister.test.js
@@ -6,14 +6,19 @@ describe('Settings persister', () => {
   beforeEach(() => {
     const initialState = readInitialState();
     store = Store(initialState);
+    subscribeToChanges(store)();
   });
 
   afterEach(() => {
     localStorage.clear();
   });
 
-  test('Do not persist nonsense values like like "undefined"', () => {
-    subscribeToChanges(store)();
-    expect(localStorage.getItem('colorScheme')).toBe(null);
+  test('Do not persist nonsense like like "false" for settings without default', () => {
+    expect(localStorage.getItem('showClockDisplay')).not.toBe('false');
+    expect(localStorage.getItem('showClockDisplay')).toBe(null);
+  });
+
+  test('Does persist given default values, for example colorScheme', () => {
+    expect(localStorage.getItem('colorScheme')).toBe('Light');
   });
 });
