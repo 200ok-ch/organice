6a0494e2edd37332123fa6e2f1f3e0b444dc6818
Alain M. Lafon <alain@200ok.ch>
2021-10-29 11:27:40 +0200

    doc: Rationale for type check

--- src/util/settings_persister.js
+++ src/util/settings_persister.js
@@ -350,6 +350,14 @@ export const loadSettingsFromConfigFile = (dispatch, getState) => {
       try {
         let config;
 
+        /* Rationale for the type check:
+          When loading the settings file, `configFileContents` is
+          sometimes already an object. Therefore, when JSON.parse is
+          run, it throws an error which is silently swallowed by the
+          catch below. This appears to be the cause of
+          https:github.com/200ok-ch/organice/issues/472. Settings will
+          never load from file and default config always loads.
+          */
         if (typeof configFileContents === 'string') {
           config = JSON.parse(configFileContents);
         } else {
