8cbeecb2866e2db5e6b966f02430dbec208fefed
Alain M. Lafon <alain@200ok.ch>
2021-11-01 17:12:49 +0100

    chore: Add GitLab application credentials to deployment automation

--- bin/compile_and_upload.sh
+++ bin/compile_and_upload.sh
@@ -10,8 +10,12 @@ set -e
 # Configure available back-end API keys
 cp .env.sample .env
 [ -z ${REACT_APP_DROPBOX_CLIENT_ID+x} ] || sed -i "s/your_dropbox_client_id/${REACT_APP_DROPBOX_CLIENT_ID}/" .env
+
 [ -z ${REACT_APP_GOOGLE_DRIVE_API_KEY+x} ] || sed -i "s/your_google_drive_api_key/${REACT_APP_GOOGLE_DRIVE_API_KEY}/" .env
 [ -z ${REACT_APP_GOOGLE_DRIVE_CLIENT_ID+x} ] || sed -i "s/your_google_drive_oauth_client_id/${REACT_APP_GOOGLE_DRIVE_CLIENT_ID}/" .env
+
+[ -z ${REACT_APP_GITLAB_CLIENT_ID+x} ] || sed -i "s/your_gitlab_client_id/${REACT_APP_GITLAB_CLIENT_ID}/" .env
+[ -z ${REACT_APP_GITLAB_SECRET+x} ] || sed -i "s/your_gitlab_secret/${REACT_APP_GITLAB_SECRET}/" .env
 
 yarn install
 yarn run build
