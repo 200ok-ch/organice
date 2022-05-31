82acbb16e6ebebe51bc221cebc029df628b11102
Phil Hofmann <phil@200ok.ch>
2022-05-31 09:52:09 +0200

    chore: setup capacitor for android and document how to use it

---
+++ android/app/src/main/java/ch/twohundredok/organice/MainActivity.java
@@ -0,0 +1,5 @@
+package ch.twohundredok.organice;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ch.twohundredok.organice',
+  appName: 'organice',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
