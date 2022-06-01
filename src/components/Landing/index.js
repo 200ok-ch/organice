77549670941e67835bfdbc0d4fc2e3d6a2ea197d
Alain M. Lafon <alain@200ok.ch>
2022-06-01 14:21:24 +0200

    fix: Youtube video responsive

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -346,18 +346,13 @@ export default () => {
 
                     <div className="col-lg-6 mb-5">
                       <div className="d-flex h-100">
-                        <div className="ms-4">
-                          <p className="text-white-50">
-                            <iframe
-                              width="560"
-                              height="315"
-                              src="https://www.youtube.com/embed/aQKc0hcFXCk"
-                              title="YouTube video player"
-                              frameBorder="0"
-                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                              allowFullScreen
-                            ></iframe>
-                          </p>
+                        <div className="tutorial container text-center my-5 ratio ratio-16x9">
+                          <iframe
+                            src="https://www.youtube.com/embed/aQKc0hcFXCk"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                          ></iframe>
                         </div>
                       </div>
                     </div>
