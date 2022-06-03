59a225f012085419370dddddce05e98c267a71b2
Alain M. Lafon <alain@200ok.ch>
2022-06-03 16:58:40 +0200

    chore: First spike into the UX of 'create new file'

---
+++ src/components/FileBrowser/components/ActionDrawer/index.js
@@ -0,0 +1,70 @@
+// INFO: There's an <ActionDrawer> component within the <OrgFile>
+// component, as well.
+
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import './../../../OrgFile/components/ActionDrawer/stylesheet.css';
+
+import * as orgActions from '../../../../actions/org';
+import * as captureActions from '../../../../actions/capture';
+import * as baseActions from '../../../../actions/base';
+
+import ActionButton from '../../../OrgFile/components/ActionDrawer/components/ActionButton';
+
+const ActionDrawer = ({ base, path }) => {
+  const handleAddNewOrgFileClick = () => {
+    base.activatePopup('addFile');
+    let fileName = prompt('New filename:');
+    console.log(fileName);
+  };
+
+  const mainButtonStyle = {
+    opacity: 1,
+    position: 'relative',
+    zIndex: 1,
+  };
+
+  return (
+    <div className="action-drawer-container nice-scroll">
+      {
+        <Fragment>
+          <div
+            className="action-drawer__capture-buttons-container"
+            style={{
+              marginLeft: 'auto',
+              marginRight: 0,
+            }}
+          >
+            <ActionButton
+              iconName="plus"
+              isDisabled={false}
+              onClick={handleAddNewOrgFileClick}
+              style={mainButtonStyle}
+              tooltip="Add new Org file"
+            />
+          </div>
+        </Fragment>
+      }
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const path = state.org.present.get('path');
+  // const files = state.org.present.get('files');
+  return {
+    path,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    org: bindActionCreators(orgActions, dispatch),
+    capture: bindActionCreators(captureActions, dispatch),
+    base: bindActionCreators(baseActions, dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActionDrawer);
