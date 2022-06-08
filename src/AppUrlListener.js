187ae944b2f08bc6c9bfad3ad8ed0c0afcc2890e
Alain M. Lafon <alain@200ok.ch>
2022-06-08 18:07:11 +0200

    doc: Rationale for passing OAuth parameters (query or hash)

--- src/AppUrlListener.js
+++ src/AppUrlListener.js
@@ -8,15 +8,25 @@ const AppUrlListener = () => {
   // let history = useHistory();
   useEffect(() => {
     App.addListener('appUrlOpen', (event) => {
+      // Some OAuth providers (i.e. GitLab) use query parameters.
+      // Migrate them from the event.url to the actual
+      // window.location.
       const newUrl = new URL(window.location.href);
       for (const entry of new URL(event.url).searchParams.entries()) {
         newUrl.searchParams.set(entry[0], entry[1]);
       }
+      // Some OAuth providers (i.e. Dropbox) use the hash. Migrate it
+      // from the event.url to window.location.
       newUrl.hash = new URL(event.url).hash;
       window.history.pushState(null, '', newUrl);
       handleAuthenticatedSyncService(readInitialState());
 
-      // window.location.reload();
+      // HACK: After the above code, the Dropbox login has succeeded.
+      // However, the constructor in App.js has to run. For some
+      // reason, this implicitly happens for GitLab. This could most
+      // definitively be implemented in a more readable manner by
+      // refactoring the App.js constructor.
+      if (newUrl.hash) window.location.reload();
 
       // // Example url: https://beerswift.app/tabs/tab2
       // // slug = /tabs/tab2
