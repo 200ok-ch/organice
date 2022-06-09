135c86e0c31d6c29708f0f0d60ea86e96f205f6e
Phil Hofmann <phil@200ok.ch>
2022-06-09 14:42:52 +0200

    chore: fix linting errors

--- src/util/redirect_url.js
+++ src/util/redirect_url.js
@@ -2,7 +2,7 @@ import { Capacitor } from '@capacitor/core';
 
 export const redirectUrl = () => {
   // https://capacitorjs.com/docs/core-apis/web#isnativeplatform
-  if(Capacitor.isNativePlatform()) return 'https://organice.200ok.ch/'
+  if (Capacitor.isNativePlatform()) return 'https://organice.200ok.ch/'
 
-  return window.location.origin + '/'
+  return window.location.origin + '/';
 };
