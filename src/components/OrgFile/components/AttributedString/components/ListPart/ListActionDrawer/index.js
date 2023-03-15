168e9fa575275cb5632d27828f4abca9655a84ae
Koji Matsuda <kjmatsuda1985@gmail.com>
2023-03-15 14:05:16 +0900

    fix style of list manipulate buttons

--- src/components/OrgFile/components/AttributedString/components/ListPart/ListActionDrawer/index.js
+++ src/components/OrgFile/components/AttributedString/components/ListPart/ListActionDrawer/index.js
@@ -3,20 +3,40 @@ import React from 'react';
 import './stylesheet.css';
 
 export default ({
-  subPartDataAndHandlers: { onEnterListEditMode, onAddNewListItem, onRemoveListItem },
+  subPartDataAndHandlers: {
+    onEnterListTitleEditMode,
+    onEnterListContentsEditMode,
+    onAddNewListItem,
+    onRemoveListItem,
+  },
 }) => {
   return (
     <div className="list-action-drawer-container">
-      <div className=" list-action-drawer__edit-icon-container" onClick={onEnterListEditMode}>
-        <i className="fas fa-pencil-alt fa-lg" />
-      </div>
+      <div className="list-action-drawer__row">
+        <div className="list-action-drawer__edit-icon-container" onClick={onEnterListTitleEditMode}>
+          <i className="fas fa-pencil-alt fa-lg" />
+        </div>
 
-      <div className="list-action-drawer__edit-icon-container" onClick={onAddNewListItem}>
-        <i className="fas fa-plus fa-lg list-action-drawer__main-icon" />
-      </div>
+        <span className="list-action-drawer__separator" />
 
-      <div className="list-action-drawer__edit-icon-container" onClick={onRemoveListItem}>
-        <i className="fas fa-times fa-lg list-action-drawer__main-icon" />
+        <div
+          className="list-action-drawer__edit-icon-container"
+          onClick={onEnterListContentsEditMode}
+        >
+          <i className="fas fa-edit fa-lg" />
+        </div>
+
+        <span className="list-action-drawer__separator" />
+
+        <div className="list-action-drawer__edit-icon-container" onClick={onAddNewListItem}>
+          <i className="fas fa-plus fa-lg" />
+        </div>
+
+        <span className="list-action-drawer__separator" />
+
+        <div className="list-action-drawer__edit-icon-container" onClick={onRemoveListItem}>
+          <i className="fas fa-times fa-lg" />
+        </div>
       </div>
     </div>
   );
