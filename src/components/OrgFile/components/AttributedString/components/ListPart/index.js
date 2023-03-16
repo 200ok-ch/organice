bdcd42dd4ad97393048d09a6a33a7dd9db46a237
Koji Matsuda <kjmatsuda1985@gmail.com>
2023-03-17 07:45:29 +0900

    Fix the bug that list contents is deleted when focus out

--- src/components/OrgFile/components/AttributedString/components/ListPart/index.js
+++ src/components/OrgFile/components/AttributedString/components/ListPart/index.js
@@ -53,6 +53,7 @@ export default class ListPart extends PureComponent {
         onListTitleValueUpdate(selectedListItemId, listTitleValues.get(selectedListItemId));
       }
     }
+
     const prevSelectedListItemId = prevProps.subPartDataAndHandlers.selectedListItemId;
     if (
       prevSelectedListItemId !== selectedListItemId &&
@@ -67,6 +68,19 @@ export default class ListPart extends PureComponent {
     if (prevProps.subPartDataAndHandlers.inListContentsEditMode && !inListContentsEditMode) {
       if (listContentsValues.has(selectedListItemId)) {
         onListContentsValueUpdate(selectedListItemId, listContentsValues.get(selectedListItemId));
+      }
+    }
+
+    if (
+      prevSelectedListItemId !== selectedListItemId &&
+      inListContentsEditMode &&
+      prevProps.subPartDataAndHandlers.inListContentsEditMode
+    ) {
+      if (listContentsValues.has(prevSelectedListItemId)) {
+        onListContentsValueUpdate(
+          prevSelectedListItemId,
+          listContentsValues.get(prevSelectedListItemId)
+        );
       }
     }
 
