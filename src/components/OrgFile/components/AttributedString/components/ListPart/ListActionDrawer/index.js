0c6329dfd6d937bd5e5968b184aafbe6f729a233
Alain M. Lafon <alain@200ok.ch>
2023-04-03 14:22:39 +0200

    doc: List manipulation functions

--- src/components/OrgFile/components/AttributedString/components/ListPart/ListActionDrawer/index.js
+++ src/components/OrgFile/components/AttributedString/components/ListPart/ListActionDrawer/index.js
@@ -14,7 +14,7 @@ export default ({
     <div className="list-action-drawer-container">
       <div className="list-action-drawer__row">
         <div className="list-action-drawer__edit-icon-container" onClick={onEnterListTitleEditMode}>
-          <i className="fas fa-pencil-alt fa-lg" />
+          <i className="fas fa-pencil-alt fa-lg" title="Edit list item title" />
         </div>
 
         <span className="list-action-drawer__separator" />
@@ -22,6 +22,7 @@ export default ({
         <div
           className="list-action-drawer__edit-icon-container"
           onClick={onEnterListContentsEditMode}
+          title="Edit list item contents"
         >
           <i className="fas fa-edit fa-lg" />
         </div>
@@ -29,13 +30,13 @@ export default ({
         <span className="list-action-drawer__separator" />
 
         <div className="list-action-drawer__edit-icon-container" onClick={onAddNewListItem}>
-          <i className="fas fa-plus fa-lg" />
+          <i className="fas fa-plus fa-lg" title="Create list item below" />
         </div>
 
         <span className="list-action-drawer__separator" />
 
         <div className="list-action-drawer__edit-icon-container" onClick={onRemoveListItem}>
-          <i className="fas fa-times fa-lg" />
+          <i className="fas fa-times fa-lg" title="Delete list item" />
         </div>
       </div>
     </div>
