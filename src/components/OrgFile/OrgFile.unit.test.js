7c3ffe2f3c66b9c7fb35709108d18e55cb2a25ad
VictorHugoPilled <echase3@gmail.com>
2024-07-08 18:57:55 +0000

    Added test fixtures and logdone tests

--- src/components/OrgFile/OrgFile.unit.test.js
+++ src/components/OrgFile/OrgFile.unit.test.js
@@ -470,6 +470,31 @@ ${description}`;
         expect(noLogRepeatEnabledP({ state, headerIndex: 7 })).toBe(true);
       });
     });
+      describe('"logdone" configuration', () => {
+	  test('Detects "logdone" when set in #+STARTUP as only option', () => {
+              const testOrgFile = readFixture('schedule_with_logdone');
+              const state = parseOrg(testOrgFile);
+              expect(noLogdoneEnabledP({ state, headerIndex: 0 })).toBe(true);
+	  });
+	  test('Detects "logdone" when set in #+STARTUP with other options', () => {
+	      const testOrgFile = readFixture('schedule_with_logdone_and_other_options');
+              const state = parseOrg(testOrgFile);
+              expect(noLogdoneEnabledP({ state, headerIndex: 0 })).toBe(true);
+	  });
+	  test('Does not detect "logdone" when not set', () => {
+              const testOrgFile = readFixture('schedule');
+              const state = parseOrg(testOrgFile);
+              expect(noLogdoneEnabledP({ state, headerIndex: 0 })).toBe(false);
+	  });
+	  test('Detects "logdone" when set via a property list', () => {
+              const testOrgFile = readFixture('schedule_with_logdone_property');
+              const state = parseOrg(testOrgFile);
+              expect(noLogdoneEnabledP({ state, headerIndex: 1 })).toBe(true);
+              expect(noLogdoneEnabledP({ state, headerIndex: 2 })).toBe(true);
+              expect(noLogdoneEnabledP({ state, headerIndex: 5 })).toBe(false);
+              expect(noLogdoneEnabledP({ state, headerIndex: 7 })).toBe(true);
+      });
+      });
   });
 
   describe('TODO keywords at EOF', () => {
