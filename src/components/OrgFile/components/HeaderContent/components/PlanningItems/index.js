51b570f66ea82fc7cb5cf2ab9dceca378c1244c6
Alain M. Lafon <alain@200ok.ch>
2025-07-16 15:23:52 +0200

    refactor: `<PlanningItems>`

--- src/components/OrgFile/components/HeaderContent/components/PlanningItems/index.js
+++ src/components/OrgFile/components/HeaderContent/components/PlanningItems/index.js
@@ -1,43 +1,38 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback } from 'react';
 
 import './stylesheet.css';
 
 import { renderAsText } from '../../../../../../lib/timestamps';
 import { isRegularPlanningItem } from '../../../../../../lib/org_utils';
 
-import _ from 'lodash';
+const PlanningItems = React.memo(({ planningItems, onClick }) => {
+  const handleTimestampClick = useCallback(
+    (planningType, planningItemIndex) => {
+      return () => onClick(planningType, planningItemIndex);
+    },
+    [onClick]
+  );
 
-export default class PlanningItems extends PureComponent {
-  constructor(props) {
-    super(props);
+  const planningItemsToRender = planningItems.filter((x) => isRegularPlanningItem(x));
+  if (planningItemsToRender.isEmpty()) return null;
 
-    _.bindAll(this, ['handleTimestampClick']);
-  }
-
-  handleTimestampClick(planningType, planningItemIndex) {
-    return () => this.props.onClick(planningType, planningItemIndex);
-  }
-
-  render() {
-    const { planningItems } = this.props;
-
-    const planningItemsToRender = planningItems.filter((x) => isRegularPlanningItem(x));
-    if (planningItemsToRender.isEmpty()) return null;
-
-    return (
-      <div>
-        {planningItemsToRender.map((planningItem, index) => (
-          <div key={planningItem.get('id')} className="planning-items__item-container">
-            <div className="planning-item__type">{planningItem.get('type')}: </div>
-            <div
-              className="planning-item__timestamp"
-              onClick={this.handleTimestampClick(planningItem.get('type'), index)}
-            >
-              {renderAsText(planningItem.get('timestamp'))}
-            </div>
+  return (
+    <div>
+      {planningItemsToRender.map((planningItem, index) => (
+        <div key={planningItem.get('id')} className="planning-items__item-container">
+          <div className="planning-item__type">{planningItem.get('type')}: </div>
+          <div
+            className="planning-item__timestamp"
+            onClick={handleTimestampClick(planningItem.get('type'), index)}
+          >
+            {renderAsText(planningItem.get('timestamp'))}
           </div>
-        ))}
-      </div>
-    );
-  }
-}
+        </div>
+      ))}
+    </div>
+  );
+});
+
+PlanningItems.displayName = 'PlanningItems';
+
+export default PlanningItems;
