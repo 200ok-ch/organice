948cef33568bbf5302b380d82bfdc4a34cff443c
Alain M. Lafon <alain@200ok.ch>
2025-07-16 14:37:33 +0200

    refactor: `<ShortcutRow>`

--- src/components/KeyboardShortcutsEditor/components/ShortcutRow/index.js
+++ src/components/KeyboardShortcutsEditor/components/ShortcutRow/index.js
@@ -1,84 +1,14 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 
 import './stylesheet.css';
 
-import _ from 'lodash';
 import classNames from 'classnames';
 
-export default class ShortcutRow extends PureComponent {
-  constructor(props) {
-    super(props);
+const ShortcutRow = ({ name, binding, onBindingChange }) => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const rebindAreaRef = useRef(null);
 
-    _.bindAll(this, ['handleRebindClick', 'handleKeyPress']);
-
-    this.state = {
-      isEditMode: false,
-    };
-  }
-
-  componentDidMount() {
-    this.rebindArea.addEventListener('blur', () => this.setState({ isEditMode: false }));
-  }
-
-  componentWillUnmount() {
-    // Clean up any remaining event listeners to prevent memory leaks
-    document.removeEventListener('keypress', this.handleKeyPress);
-  }
-
-  handleRebindClick() {
-    this.setState({ isEditMode: true });
-    document.addEventListener('keypress', this.handleKeyPress);
-  }
-
-  handleKeyPress(event) {
-    this.setState({ isEditMode: false });
-    document.removeEventListener('keypress', this.handleKeyPress);
-
-    let key = null;
-    if (event.code.startsWith('Key')) {
-      key = event.code.substr(3).toLowerCase();
-    } else if (event.code.startsWith('Digit')) {
-      key = event.code.substr(5);
-    } else {
-      key = {
-        Minus: '-',
-        Equal: '=',
-        Backspace: 'backspace',
-        Enter: 'enter',
-        Return: 'return',
-        Tab: 'tab',
-        BracketLeft: '[',
-        BracketRight: ']',
-        Semicolon: ';',
-        Quote: '"',
-        Backslash: '\\',
-        Comma: ',',
-        Period: '.',
-        Slash: '/',
-      }[event.code];
-    }
-
-    if (!key) {
-      return;
-    }
-
-    const modifiers = [
-      ['ctrlKey', 'ctrl'],
-      ['altKey', 'alt'],
-      ['metaKey', 'meta'],
-      ['shiftKey', 'shift'],
-    ];
-
-    const newModifiers = modifiers
-      .filter(([modifier, _]) => event[modifier])
-      .map(([_, symbol]) => symbol)
-      .join('+');
-    const newBinding = `${newModifiers}${!!newModifiers ? '+' : ''}${key}`;
-
-    this.props.onBindingChange(this.props.name, newBinding);
-  }
-
-  symbolizeKeybinding(binding) {
+  const symbolizeKeybinding = useCallback((binding) => {
     if (!binding) {
       return '';
     }
@@ -103,28 +33,103 @@ export default class ShortcutRow extends PureComponent {
       (currentBinding, [name, symbol]) => currentBinding.replace(RegExp(`${name}\\+?`), symbol),
       binding
     );
-  }
+  }, []);
 
-  render() {
-    const { name, binding } = this.props;
-    const { isEditMode } = this.state;
+  const handleKeyPress = useCallback(
+    (event) => {
+      setIsEditMode(false);
+      document.removeEventListener('keypress', handleKeyPress);
 
-    const rebindAreaClassName = classNames('keyboard-shortcut-container__shortcut-key', {
-      'keyboard-shortcut-container__shortcut-key--edit-mode': isEditMode,
-    });
+      let key = null;
+      if (event.code.startsWith('Key')) {
+        key = event.code.substr(3).toLowerCase();
+      } else if (event.code.startsWith('Digit')) {
+        key = event.code.substr(5);
+      } else {
+        key = {
+          Minus: '-',
+          Equal: '=',
+          Backspace: 'backspace',
+          Enter: 'enter',
+          Return: 'return',
+          Tab: 'tab',
+          BracketLeft: '[',
+          BracketRight: ']',
+          Semicolon: ';',
+          Quote: '"',
+          Backslash: '\\',
+          Comma: ',',
+          Period: '.',
+          Slash: '/',
+        }[event.code];
+      }
 
-    return (
-      <div className="keyboard-shortcut-container">
-        <div className="keyboard-shortcut-container__shortcut-name">{name}</div>
-        <div
-          className={rebindAreaClassName}
-          onClick={this.handleRebindClick}
-          tabIndex="-1"
-          ref={(div) => (this.rebindArea = div)}
-        >
-          {isEditMode ? '...' : this.symbolizeKeybinding(binding)}
-        </div>
+      if (!key) {
+        return;
+      }
+
+      const modifiers = [
+        ['ctrlKey', 'ctrl'],
+        ['altKey', 'alt'],
+        ['metaKey', 'meta'],
+        ['shiftKey', 'shift'],
+      ];
+
+      const newModifiers = modifiers
+        .filter(([modifier, _]) => event[modifier])
+        .map(([_, symbol]) => symbol)
+        .join('+');
+      const newBinding = `${newModifiers}${!!newModifiers ? '+' : ''}${key}`;
+
+      onBindingChange(name, newBinding);
+    },
+    [name, onBindingChange]
+  );
+
+  const handleRebindClick = useCallback(() => {
+    setIsEditMode(true);
+    document.addEventListener('keypress', handleKeyPress);
+  }, [handleKeyPress]);
+
+  const handleBlur = useCallback(() => {
+    setIsEditMode(false);
+  }, []);
+
+  useEffect(() => {
+    const rebindArea = rebindAreaRef.current;
+    if (rebindArea) {
+      rebindArea.addEventListener('blur', handleBlur);
+
+      return () => {
+        rebindArea.removeEventListener('blur', handleBlur);
+      };
+    }
+  }, [handleBlur]);
+
+  useEffect(() => {
+    return () => {
+      // Clean up any remaining event listeners to prevent memory leaks
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
+  const rebindAreaClassName = classNames('keyboard-shortcut-container__shortcut-key', {
+    'keyboard-shortcut-container__shortcut-key--edit-mode': isEditMode,
+  });
+
+  return (
+    <div className="keyboard-shortcut-container">
+      <div className="keyboard-shortcut-container__shortcut-name">{name}</div>
+      <div
+        className={rebindAreaClassName}
+        onClick={handleRebindClick}
+        tabIndex="-1"
+        ref={rebindAreaRef}
+      >
+        {isEditMode ? '...' : symbolizeKeybinding(binding)}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default React.memo(ShortcutRow);
