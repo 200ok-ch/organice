5b1b7669431882037bc47adb96cc314c8d791664
Alain M. Lafon <alain@200ok.ch>
2026-01-10 00:10:34 +0100

    test: Use 2 workers on CircleCI

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
 
   // Opt out of parallel tests on CI
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
 
   // Reporter to use
   reporter: 'html',
