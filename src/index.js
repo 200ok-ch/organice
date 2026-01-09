e7636262c4098c706ed221e7e3f402147c780f98
Alain M. Lafon <alain@200ok.ch>
2026-01-09 16:32:58 +0100

    refactor: Add local Font Awesome v5.1.1 for offline support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './fontawesome.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
