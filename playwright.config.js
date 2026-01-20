d68d3945c5db4ee1fec48231ab9dfdfbf23521e1
Alain M. Lafon <alain@200ok.ch>
2026-01-20 20:24:53 +0100

    Revert "fix(e2e): Use production build with serve instead of dev server"
    This reverts commit 464ae9652c1ed2481e440521782f624b6395c843.

--- playwright.config.js
+++ playwright.config.js
@@ -70,7 +70,7 @@ export default defineConfig({
 
   // Run your local dev server before starting the tests
   webServer: {
-    command: 'yarn serve:e2e',
+    command: 'yarn start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
